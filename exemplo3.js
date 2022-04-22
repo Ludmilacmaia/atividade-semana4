bf71075dad02ef09c4a214300888b93f68c50745
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-22 13:05:31 -0300

    Resolução do exemplo 3

---
+++ exemplo3.js
@@ -0,0 +1,9 @@
+//Faça um programa que dado um número, imprime no (console.log) a tabuada do mesmo de 1 a 10.
+
+let i=1
+const tabuadaDeCinco = 5
+for (i=1; i<=10; i++){
+    console.log(`${i} x 5 = ${i*tabuadaDeCinco} `)
+}
+
+
